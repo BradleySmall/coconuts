9f1b7f773884b47d7a805a3f5a38f8829a133ad3
Bradley M. Small <bradley_small@hotmail.com>
2019-10-31 20:46:01 -0400

    making lint corrections

--- coco.py
+++ coco.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""
-Program to solve the 5 men and the coconuts problem
-"""
+"""Solve the 5 men and the coconuts problem."""
 
 
-def splitthem(quant, running_count):
-    """
-    take a quantity and return False if it is not one more than
-    equally divisible by 5
+def split_them(quant, running_count):
+    """Split quantity keeping the running count.
 
-    Otherwise, subtract 1 and subtract 1/5 of the remaining
+    Take a quantity and return False if it is not one more than
+    equally divisible by 5.
 
-    when the split is done, update the running count for whichever
+    Subtract 1 and subtract 1/5 of the remaining
+
+    When the split is done, update the running count for whichever
     first zero value
     """
     if quant[0] % 5 != 1:
@@ -26,10 +25,11 @@ def splitthem(quant, running_count):
     return True
 
 
-def testnum(num, running_count):
-    """
-    for a given number test to see if it will succeed using the
-    logic for split defined in splitthem 5 times.
+def test_num(num, running_count):
+    """Test a number for validity in this scenario.
+
+    For a given number test to see if it will succeed using the
+    logic for split defined in split_them 5 times.
 
     Then test to see that it is equally divisible by 5
 
@@ -37,41 +37,42 @@ def testnum(num, running_count):
     one last time for the final split.
     """
     for _ in range(5):
-        if not splitthem(num, running_count):
+        if not split_them(num, running_count):
             return False
 
     if num[0] % 5 == 0:
         share_amt = num[0]//5
-        for i in range(5):
-            running_count[i] += share_amt
+
+        running_count = [count + share_amt for count in running_count]
+
+        # for i in range(5):
+        #    running_count[i] += share_amt
 
         return True
     return False
 
 
 def main():
-    """
-    Main driver for the brute force solver
+    """Solve with brute force.
 
-    choose a number and see if the result can be found and return
+    Choose a number and see if the result can be found and return
     or print an error
-
     """
     test_quantity = 1000000
 
     num = [0]
-    for test_num in range(1, test_quantity):
+    for test_value in range(1, test_quantity):
         running_count = [0, 0, 0, 0, 0]
 
-        num[0] = test_num
-        if testnum(num, running_count):
-            print("The Number Was %d" % test_num)
+        num[0] = test_value
+        if test_num(num, running_count):
+            print("The Number Was %d" % test_value)
             print(running_count)
 
             total = sum(running_count)
 
             print("Total %d, plus 5 for the monkey!"
-                  " Original starting count %d" % (total, test_num))
+                  " Original starting count %d" % (total, test_value))
             return
 
     print("Not found in %d tries." % test_quantity)
